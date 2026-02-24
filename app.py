fc72304833cb041a709d5b3d5c7132fae2d76fff
plaads <lukaspedr2@gmail.com>
2026-02-24 19:51:16 -0300

    feacture: Criando o problema

---
+++ app.py
@@ -0,0 +1,14 @@
+# PROBLEMA: dados existem só enquanto o programa roda
+
+produto = "Camiseta"
+quantidade = 2
+preco = 50
+
+total = quantidade * preco
+
+print("Produto:", produto)
+print("Quantidade:", quantidade)
+print("Preço:", preco)
+print("Total da venda:", total)
+
+# Quando o programa fecha, tudo é perdido
